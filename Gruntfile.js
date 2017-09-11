79d009b418536fd373f6f4080966fa8f2c0c0e57
Kyriakos Barbounakis <k.barbounakis@gmail.com>
2017-09-11 20:52:40 +0300

    initial commit (import ES2015 modules)

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,7 @@
 /**
  * @license
  * MOST Web Framework 2.0 Codename Blueshift
- * Copyright (c) 2014, Kyriakos Barbounakis k.barbounakis@gmail.com
- *                     Anthi Oikonomou anthioikonomou@gmail.com
+ * Copyright (c) 2017, THEMOST LP All rights reserved
  *
  * Use of this source code is governed by an BSD-3-Clause license that can be
  * found in the LICENSE file at https://themost.io/license
