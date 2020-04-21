3c16302d7085b704bbb56d65c3d692b546590d99
Kyriakos Barbounakis <k.barbounakis@gmail.com>
2020-04-21 10:55:44 +0300

    update model builder test

--- spec/ODataModelBuilder.spec.js
+++ spec/ODataModelBuilder.spec.js
@@ -1,5 +1,16 @@
 import {getApplication} from '@themost/test';
-import {ODataModelBuilder} from '@themost/data';
+import path from 'path';
+import {ODataModelBuilder, FileSchemaLoaderStrategy, SchemaLoaderStrategy} from '@themost/data';
+import {XDocument} from '@themost/xml';
+
+class OtherSchemaLoader extends FileSchemaLoaderStrategy {
+    constructor(configuration) {
+        super(configuration);
+        // set explicitly model directory
+        this.setModelPath(path.resolve(__dirname, './other-models'));
+    }
+}
+
 describe('ODataModelBuilder', ()=> {
     let app;
     beforeAll(() => {
@@ -20,4 +31,43 @@ describe('ODataModelBuilder', ()=> {
         const document = builder.getEdmDocumentSync();
         expect(document).toBeTruthy();
     })
+    it('should treat readonly properties as computed', async ()=> {
+        /**
+         * @type {ExpressDataApplication}
+         */
+        const application = app.get('ExpressDataApplication');
+        // use extra loader
+        const schemaLoader = application.getConfiguration().getStrategy(SchemaLoaderStrategy);
+        schemaLoader.loaders.push(new OtherSchemaLoader(application.getConfiguration()));
+        // reload builder
+        application.useModelBuilder();
+        /**
+         * @type {ODataModelBuilder}
+         */
+        let builder = application.getStrategy(ODataModelBuilder);
+        const entity = builder.getEntity('TestEnumeration');
+        expect(entity).toBeTruthy();
+        /**
+         * @type XDocument
+         */
+        const document = builder.getEdmDocumentSync();
+        const node = document.documentElement.selectSingleNode('edmx:DataServices/Schema/EntityType[@Name="TestEnumeration"]');
+        expect(node).toBeTruthy();
+        let nodeProperty = node.selectSingleNode('Property[@Name="additionalType"]');
+        expect(nodeProperty).toBeTruthy();
+        let nodeAnnotation = nodeProperty.selectSingleNode('Annonation[@Term="Org.OData.Core.V1.Computed"]');
+        expect(nodeAnnotation).toBeTruthy();
+
+        nodeProperty = node.selectSingleNode('Property[@Name="createdBy"]');
+        expect(nodeProperty).toBeTruthy();
+        nodeAnnotation = nodeProperty.selectSingleNode('Annonation[@Term="Org.OData.Core.V1.Computed"]');
+        expect(nodeAnnotation).toBeTruthy();
+
+        nodeProperty = node.selectSingleNode('Property[@Name="id"]');
+        expect(nodeProperty).toBeTruthy();
+        nodeAnnotation = nodeProperty.selectSingleNode('Annonation[@Term="Org.OData.Core.V1.Computed"]');
+        expect(nodeAnnotation).toBeFalsy();
+        
+
+    });
 });
